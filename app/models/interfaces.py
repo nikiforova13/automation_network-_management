498e67fc8d35ccd26488434a1da34630ed405d8b
Olya Ievleva <olyanikiforova1313@gmail.com>
2024-04-16 20:42:30 +0300

    added vlans

--- app/models/interfaces.py
+++ app/models/interfaces.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 from ipaddress import IPv4Address, IPv4Network
+class Vlan(BaseModel):
+    number: int | None = None
+    mode: str | None = None
 class Interface(BaseModel):
     interface: str | None = None
     ip_address: IPv4Address | None = None
     subnet_mask: str | None = None
     status: str | None = None
     duplex: str | None = None
-    vlan: int | None = None
-    mode: str | None = None
+    vlan: Vlan | None = None
 
--- app/routers/api_schemas/configuration.py
+++ app/routers/api_schemas/configuration.py
@@ -4,9 +4,13 @@ from ipaddress import IPv4Address, IPv4Network
 from app.models.interfaces import Interface
 from app.models.routing import Routing
 
+class Vlan(BaseModel):
+    number: int | None = None
+    name: str | None = None
 
 class DeviceConfiguration(BaseModel):
     hostname: str | None = None
+    vlans: list[Vlan] | None = None
     interfaces: list[Interface] | None = None
     routing: Routing | None = None
 
