8bb2c5fce5de087be076e5b6e103e3031942b4b0
Olya Ievleva <olyanikiforova1313@gmail.com>
2024-05-11 18:10:34 +0300

    refactoring

--- app/ui_stub/index.py
+++ app/ui_stub/index.py
@@ -1,14 +1,9 @@
-import pathlib
-
 from fastapi import APIRouter
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-
-APP_GLOBAL_PATH = pathlib.Path(__file__).absolute().parent.parent.joinpath("templates")
+from app.config.device_config import templates
 
 router = APIRouter(prefix="")
-templates = Jinja2Templates(directory=APP_GLOBAL_PATH)
 
 
 @router.get("/", response_class=HTMLResponse)
